2e1946b06be4be411aa1b634f82c9cbbec1d3a9e
Gregory Boissinot <gregory.boissinot@gmail.com>
2014-10-08 14:52:12 +0200

    Add type with primary

--- Libs/LibMongoPomImporter/src/main/java/fr/synchrotron/soleil/ica/ci/lib/mongodb/pomimporter/service/ArtifactDocumentLoaderService.java
+++ Libs/LibMongoPomImporter/src/main/java/fr/synchrotron/soleil/ica/ci/lib/mongodb/pomimporter/service/ArtifactDocumentLoaderService.java
@@ -78,6 +78,9 @@ class ArtifactDocumentLoaderService {
 
         artifactDocument.setBuildContext(buildContext);
 
+
+        artifactDocument.setType("primary");
+
         return artifactDocument;
     }
 
